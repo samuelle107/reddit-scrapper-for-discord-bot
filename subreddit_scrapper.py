ca3c649daf29178e477f84249eec1f37cec99518
Samuel Le <samuelle@Samuels-MacBook-Pro.local>
2020-04-16 04:23:55 -0500

    fix casing

--- subreddit_scrapper.py
+++ subreddit_scrapper.py
@@ -17,7 +17,7 @@ class SubredditScraper:
     def get_scraped_submissions(self, tracked_subreddit, keywords):
         try:
             return list(filter(
-                    lambda submission: any(keyword in submission.title.lower() for keyword in keywords) and '[US' in submission.title,
+                    lambda submission: any(keyword in submission.title.lower() for keyword in keywords) and '[us' in submission.title.lower(),
                     self.reddit.subreddit(tracked_subreddit).new(limit=10)
             ))
         except:
