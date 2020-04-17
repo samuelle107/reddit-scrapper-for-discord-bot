29fd0f0b7d2e5d3950a65fbebf5cc35ba1496d8d
Samuel Le <samuelle@Samuels-MacBook-Pro.local>
2020-04-17 15:39:41 -0500

    revert

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 from tinydb import TinyDB, Query, where
 
 # Constants
+tracked_subreddit = 'MechMarket'
 channel_id = 700110066044633169
 client = commands.Bot(command_prefix = '!')
 discord_bot_token = os.environ['DISCORD_BOT_TOKEN']
@@ -17,12 +18,10 @@ db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
 # Tiny DB tables
 submission_table = db.table('post_table')
 keyword_table = db.table('keyword_table')
-subreddit_table = db.table('subreddit_table')
 
 # Tiny DB queries
 submission_query = Query()
 keyword_query = Query()
-subreddit_query = Query()
 
 @client.event
 async def on_ready():
@@ -32,13 +31,12 @@ async def on_ready():
     await channel.send('I am ready!')
 
     while True:
-        for subreddit in [d['subreddit'] for d in subreddit_table.all()]:
-            for submission in scraper.get_scraped_submissions(subreddit, [d['keyword'] for d in keyword_table.all()]):
-                if not submission_table.search(submission_query.id == submission.id):
-                    submission_table.insert({ 'id': submission.id })
-                    await channel.send(submission.url)
+        for submission in scraper.get_scraped_submissions(tracked_subreddit, [d['keyword'] for d in keyword_table.all()]):
+            if not submission_table.search(submission_query.id == submission.id):
+                submission_table.insert({ 'id': submission.id })
+                await channel.send(submission.url)
 
-            await asyncio.sleep(60)
+        await asyncio.sleep(60)
 
 @client.command()
 async def add_keywords(ctx, *arg):
@@ -57,33 +55,16 @@ async def remove_keyword(ctx, arg):
     await ctx.send(f'Sucessfully removed {arg}')
 
 @client.command()
-async def add_subreddits(ctx, *arg):
-    for s in arg:
-        subreddit_table.insert({ 'subreddit': s })
-        await ctx.send(f'Sucessfully added {s}')
-
-@client.command()
-async def get_subreddits(ctx):
-    await ctx.send([d['subreddit'] for d in subreddit_table.all()])
-
-@client.command()
-async def remove_subreddit(ctx, arg):
-    subreddit_table.remove(where('subreddit') == arg)
-
-    await ctx.send(f'Sucessfully removed {arg}')
-
-@client.command()
 async def get_now(ctx):
     scraper = subreddit_scrapper.SubredditScraper()
 
     foundInterest = False
 
-    for subreddit in [d['subreddit'] for d in submission_table.all()]:
-        for submission in scraper.get_scraped_submissions(subreddit, [d['keyword'] for d in keyword_table.all()]):
-            if not submission_table.search(submission_query.id == submission.id):
-                foundInterest = True
-                submission_table.insert({ 'id': submission.id })
-                await ctx.send(submission.url)
+    for submission in scraper.get_scraped_submissions(tracked_subreddit, [d['keyword'] for d in keyword_table.all()]):
+        if not submission_table.search(submission_query.id == submission.id):
+            foundInterest = True
+            submission_table.insert({ 'id': submission.id })
+            await ctx.send(submission.url)
     
     if not foundInterest:
         await ctx.send('Nothing of interest to be found')
