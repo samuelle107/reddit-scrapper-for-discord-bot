d1b94a3dc99d08345ed0a72c967ec316c8ec5993
Samuel Le <samuelle107@gmail.com>
2020-04-20 11:13:43 -0500

    Remove quotes from titles

--- db_helper.py
+++ db_helper.py
@@ -3,52 +3,67 @@ import logging
 
 # Will insert a row into the table with the corresponding rows and columns
 def insert(connection, table_name, columns, values):
+    cursor = connection.cursor()
+
     try:
         join_str = "', '"
         tick_str = "'"
 
-        cursor = connection.cursor()
         query = f"INSERT INTO {table_name}({', '.join(columns)}) VALUES({tick_str + join_str.join(values) + tick_str})"
         cursor.execute(query)
         connection.commit()
         cursor.close()
-    except:
+    except Exception as e:
         logging.error(f'{str(datetime.datetime.now())}: Error inserting')
+        logging.error(f'{str(datetime.datetime.now())}: {e}')
+        cursor.close()
 
 # Given a column and value, check if that row exists in the table
 def does_exist(connection, table_name, column, value):
+    cursor = connection.cursor()
+
     try:
-        cursor = connection.cursor()
         query = f"SELECT exists(SELECT * from {table_name} where {column} = '{value}')"
         cursor.execute(query)
         does_exist =  cursor.fetchone()[0]
         cursor.close()
+
         return does_exist
-    except:
+    except Exception as e:
         logging.error(f'{str(datetime.datetime.now())}: Error checking existence')
+        logging.error(f'{str(datetime.datetime.now())}: {e}')
+        cursor.close()
+
         return False
 
 # Will remove a row base on column = value
 def remove(connection, table_name, column, value):
+    cursor = connection.cursor()
+
     try:
-        cursor = connection.cursor()
         query = f"DELETE FROM {table_name} where {column} = '{value}'"
         cursor.execute(query)
         connection.commit()
         cursor.close()
-    except:
+    except Exception as e:
         logging.error(f'{str(datetime.datetime.now())}: Error removing')
+        logging.error(f'{str(datetime.datetime.now())}: {e}')
+        cursor.close()
 
 # Will return a list of tuples of all the rows in the corresponding table
 def query_all(connection, table_name):
+    cursor = connection.cursor()
+
     try:
-        cursor = connection.cursor()
         query = f"SELECT * FROM {table_name}"
         cursor.execute(query)
         result = cursor.fetchall()
         cursor.close()
 
         return result
-    except:
+    except Exception as e:
         logging.error(f'{str(datetime.datetime.now())}: Error querying all')
+        logging.error(f'{str(datetime.datetime.now())}: {e}')
+        cursor.close()
+
         return []
